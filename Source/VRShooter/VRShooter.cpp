4e60fcaca842c2a71e4435a8e6a3fc3a7304f736
Кривошея Михаил <mrkriv@bk.ru>
2018-03-30 21:07:04 +0300

    Удалил лишние cpp файлы

--- Source/VRShooter/VRShooter.cpp
+++ Source/VRShooter/VRShooter.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "VRShooter.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRShooter, "VRShooter" );
